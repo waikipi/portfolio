1c784dc7cc0b18f028431662696ecc6a16721e85
waikipi <galianez@gmail.com>
2023-03-14 19:53:11 +0100

    url updated

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -19,7 +19,7 @@ export default function BaseLayout() {
                <Routes>
                   <Route exact path={'/'} element={<Home/>}/>
                   <Route exact path={'/about'} element={<About/>}/>
-                  <Route exact path={'/projects'} element={<Portfolio/>}/>
+                  <Route exact path={'/portfolio'} element={<Portfolio/>}/>
                </Routes>
             </Grid>
          </Grid>
