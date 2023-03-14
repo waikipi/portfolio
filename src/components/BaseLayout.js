e5f050d196a516e699627cad5bbd89ffc3b30661
waikipi <galianez@gmail.com>
2023-03-14 20:05:00 +0100

    url and homepage modified

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -17,7 +17,7 @@ export default function BaseLayout() {
             </Grid>
             <Grid item flexGrow={1}>
                <Routes>
-                  <Route exact path={'/'} element={<Home/>}/>
+                  <Route exact path={'/home'} element={<Home/>}/>
                   <Route exact path={'/about'} element={<About/>}/>
                   <Route exact path={'/projects'} element={<Portfolio/>}/>
                </Routes>
